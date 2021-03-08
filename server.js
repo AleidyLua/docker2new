55bcbf2973736a2ff84d1de6044af25ed9c4796d
Aleidy <macbook@Aleidys-Air.home>
2021-03-08 15:57:37 -0600

    st

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello test 3 ');
+    res.send('my app 2 staging ');
 });
 
 app.listen(PORT, HOST);
